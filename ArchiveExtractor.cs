72a1500bc4b1731c554d3376c1671c57cb741c72
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-05-23 14:59:40 -0500

    Create ArchiveExtractor.cs

---
+++ ArchiveExtractor.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using System.IO;
+
+namespace ProcessorEmulator.Tools
+{
+    public static class ArchiveExtractor
+    {
+        public static void ExtractArchive(string archivePath, string outputDir)
+        {
+            // Assumes 7z.exe is in PATH or same directory as the app
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "7z.exe",
+                    Arguments = $"x \"{archivePath}\" -o\"{outputDir}\" -y",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
