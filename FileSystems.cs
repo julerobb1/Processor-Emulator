6ed34ce712861f2902a9912feccc27b13cae329b
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-05-25 13:22:34 -0500

    Create FileSystems.cs

---
+++ FileSystems.cs
@@ -0,0 +1,282 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ProcessorEmulator.Tools.FileSystems
+{
+    public class JFFS2Implementation
+    {
+        private const uint JFFS2_MAGIC_BITMASK = 0x1985;
+        private const uint NODE_HEADER_SIZE = 12;
+        
+        private struct JFFS2_NodeHeader
+        {
+            public uint Magic;      // JFFS2 magic number
+            public uint Type;       // Type of node
+            public uint Length;     // Length of data
+            public uint Checksum;   // Node checksum
+        }
+
+        public class JFFS2_FileSystem
+        {
+            private Dictionary<string, byte[]> fileData = new Dictionary<string, byte[]>();
+            private Dictionary<string, JFFS2_NodeHeader> nodeHeaders = new Dictionary<string, JFFS2_NodeHeader>();
+
+            public void ParseImage(byte[] imageData)
+            {
+                int offset = 0;
+                while (offset < imageData.Length)
+                {
+                    var header = ReadNodeHeader(imageData, ref offset);
+                    if (header.Magic != JFFS2_MAGIC_BITMASK)
+                        throw new Exception("Invalid JFFS2 node found");
+
+                    ProcessNode(header, imageData, ref offset);
+                }
+            }
+
+            private JFFS2_NodeHeader ReadNodeHeader(byte[] data, ref int offset)
+            {
+                JFFS2_NodeHeader header = new JFFS2_NodeHeader
+                {
+                    Magic = BitConverter.ToUInt32(data, offset),
+                    Type = BitConverter.ToUInt32(data, offset + 4),
+                    Length = BitConverter.ToUInt32(data, offset + 8),
+                    Checksum = BitConverter.ToUInt32(data, offset + 12)
+                };
+                offset += NODE_HEADER_SIZE;
+                return header;
+            }
+
+            private void ProcessNode(JFFS2_NodeHeader header, byte[] data, ref int offset)
+            {
+                byte[] nodeData = new byte[header.Length];
+                Array.Copy(data, offset, nodeData, 0, (int)header.Length);
+                
+                switch (header.Type)
+                {
+                    case 0x1000: // JFFS2_NODETYPE_INODE
+                        ProcessInodeNode(nodeData);
+                        break;
+                    case 0x2000: // JFFS2_NODETYPE_DIRENT
+                        ProcessDirentNode(nodeData);
+                        break;
+                }
+                
+                offset += (int)header.Length;
+            }
+
+            private void ProcessInodeNode(byte[] nodeData)
+            {
+                // Implement inode processing
+            }
+
+            private void ProcessDirentNode(byte[] nodeData)
+            {
+                // Implement directory entry processing
+            }
+
+            public byte[] ReadFile(string path)
+            {
+                if (fileData.TryGetValue(path, out byte[] data))
+                    return data;
+                throw new FileNotFoundException();
+            }
+
+            public void WriteFile(string path, byte[] data)
+            {
+                fileData[path] = data;
+                // Update node headers and filesystem structure
+            }
+        }
+    }
+
+    public class YAFFSImplementation
+    {
+        private const uint YAFFS_MAGIC = 0x59414653;  // "YAFS"
+        
+        private struct YAFFSHeader
+        {
+            public uint Magic;
+            public uint Version;
+            public uint PageSize;
+            public uint SpareSize;
+        }
+
+        public class YAFFS_FileSystem
+        {
+            private Dictionary<int, byte[]> chunks = new Dictionary<int, byte[]>();
+            private Dictionary<string, List<int>> fileChunks = new Dictionary<string, List<int>>();
+
+            public void ParseImage(byte[] imageData)
+            {
+                var header = ParseHeader(imageData);
+                if (header.Magic != YAFFS_MAGIC)
+                    throw new Exception("Invalid YAFFS format");
+
+                ParseChunks(imageData, header);
+            }
+
+            private YAFFSHeader ParseHeader(byte[] data)
+            {
+                return new YAFFSHeader
+                {
+                    Magic = BitConverter.ToUInt32(data, 0),
+                    Version = BitConverter.ToUInt32(data, 4),
+                    PageSize = BitConverter.ToUInt32(data, 8),
+                    SpareSize = BitConverter.ToUInt32(data, 12)
+                };
+            }
+
+            private void ParseChunks(byte[] data, YAFFSHeader header)
+            {
+                int offset = 16; // After header
+                while (offset < data.Length)
+                {
+                    // Read chunk data and metadata
+                    int chunkId = BitConverter.ToInt32(data, offset);
+                    int dataSize = (int)header.PageSize;
+                    byte[] chunkData = new byte[dataSize];
+                    Array.Copy(data, offset + 4, chunkData, 0, dataSize);
+                    chunks[chunkId] = chunkData;
+                    offset += 4 + dataSize + (int)header.SpareSize;
+                }
+            }
+
+            public byte[] ReadFile(string path)
+            {
+                if (!fileChunks.ContainsKey(path))
+                    throw new FileNotFoundException();
+
+                List<byte[]> fileData = new List<byte[]>();
+                foreach (int chunkId in fileChunks[path])
+                {
+                    fileData.Add(chunks[chunkId]);
+                }
+
+                return CombineChunks(fileData);
+            }
+
+            private byte[] CombineChunks(List<byte[]> chunks)
+            {
+                int totalSize = chunks.Sum(chunk => chunk.Length);
+                byte[] result = new byte[totalSize];
+                int offset = 0;
+                foreach (byte[] chunk in chunks)
+                {
+                    Array.Copy(chunk, 0, result, offset, chunk.Length);
+                    offset += chunk.Length;
+                }
+                return result;
+            }
+        }
+    }
+
+    public class UFSImplementation
+    {
+        private const uint UFS_MAGIC = 0x00011954;
+        
+        private struct UFSSuperblock
+        {
+            public uint Magic;
+            public uint BlockSize;
+            public uint FragmentSize;
+            public uint NumInodes;
+        }
+
+        public class UFS_FileSystem
+        {
+            private UFSSuperblock superblock;
+            private Dictionary<uint, byte[]> blocks = new Dictionary<uint, byte[]>();
+            private Dictionary<uint, UFSInode> inodes = new Dictionary<uint, UFSInode>();
+
+            private struct UFSInode
+            {
+                public uint Mode;
+                public uint Size;
+                public uint[] BlockPointers;
+            }
+
+            public void ParseImage(byte[] imageData)
+            {
+                superblock = ParseSuperblock(imageData);
+                if (superblock.Magic != UFS_MAGIC)
+                    throw new Exception("Invalid UFS format");
+
+                ParseInodes(imageData);
+                ParseBlocks(imageData);
+            }
+
+            private UFSSuperblock ParseSuperblock(byte[] data)
+            {
+                return new UFSSuperblock
+                {
+                    Magic = BitConverter.ToUInt32(data, 0),
+                    BlockSize = BitConverter.ToUInt32(data, 4),
+                    FragmentSize = BitConverter.ToUInt32(data, 8),
+                    NumInodes = BitConverter.ToUInt32(data, 12)
+                };
+            }
+
+            private void ParseInodes(byte[] data)
+            {
+                int offset = 8192; // Standard UFS superblock size
+                for (uint i = 0; i < superblock.NumInodes; i++)
+                {
+                    UFSInode inode = new UFSInode
+                    {
+                        Mode = BitConverter.ToUInt32(data, offset),
+                        Size = BitConverter.ToUInt32(data, offset + 4),
+                        BlockPointers = new uint[15] // UFS uses 15 block pointers
+                    };
+
+                    for (int j = 0; j < 15; j++)
+                    {
+                        inode.BlockPointers[j] = BitConverter.ToUInt32(data, offset + 8 + (j * 4));
+                    }
+
+                    inodes[i] = inode;
+                    offset += 128; // Standard UFS inode size
+                }
+            }
+
+            private void ParseBlocks(byte[] data)
+            {
+                int blockOffset = 8192 + (int)(superblock.NumInodes * 128);
+                int blockSize = (int)superblock.BlockSize;
+
+                for (int i = 0; blockOffset < data.Length; i++)
+                {
+                    byte[] block = new byte[blockSize];
+                    Array.Copy(data, blockOffset, block, 0, Math.Min(blockSize, data.Length - blockOffset));
+                    blocks[(uint)i] = block;
+                    blockOffset += blockSize;
+                }
+            }
+
+            public byte[] ReadFile(uint inodeNumber)
+            {
+                if (!inodes.ContainsKey(inodeNumber))
+                    throw new FileNotFoundException();
+
+                UFSInode inode = inodes[inodeNumber];
+                byte[] fileData = new byte[inode.Size];
+                int offset = 0;
+
+                foreach (uint blockPointer in inode.BlockPointers)
+                {
+                    if (blockPointer == 0 || offset >= inode.Size)
+                        break;
+
+                    byte[] block = blocks[blockPointer];
+                    int copySize = Math.Min(block.Length, (int)inode.Size - offset);
+                    Array.Copy(block, 0, fileData, offset, copySize);
+                    offset += copySize;
+                }
+
+                return fileData;
+            }
+        }
+    }
+}
