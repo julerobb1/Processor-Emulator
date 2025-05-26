7dd7dd970b35eda65c895cb1797b5f19d2e67cff
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-05-26 18:15:54 -0500

    Update MipsCpuEmulator.cs

--- MipsCpuEmulator.cs
+++ MipsCpuEmulator.cs
@@ -11,10 +11,12 @@ namespace ProcessorEmulator.Emulation
         private uint[] registers;
         private byte[] memory;
         private uint programCounter;
+        private float[] floatingPointRegisters;
 
         public MipsCpuEmulator()
         {
             registers = new uint[RegisterCount];
+            floatingPointRegisters = new float[RegisterCount];
             memory = new byte[MemorySize];
             programCounter = 0x0;
         }
@@ -49,20 +51,18 @@ namespace ProcessorEmulator.Emulation
                 case 0x00: // R-type instructions
                     ExecuteRType(instruction);
                     break;
-                case 0x23: // lw
-                    ExecuteLoadWord(instruction);
+                case 0x11: // COP1 (Floating-point instructions)
+                    ExecuteFloatingPoint(instruction);
                     break;
-                case 0x2B: // sw
-                    ExecuteStoreWord(instruction);
+                case 0x1C: // DSP instructions
+                    ExecuteDSPInstruction(instruction);
                     break;
-                case 0x04: // beq
-                    ExecuteBranchEqual(instruction);
-                    break;
-                case 0x05: // bne
-                    ExecuteBranchNotEqual(instruction);
+                case 0x10: // System instructions
+                    ExecuteSystemInstruction(instruction);
                     break;
                 default:
-                    throw new NotSupportedException($"Opcode {opcode:X2} not supported.");
+                    HandleException($"Unsupported opcode: {opcode:X2}");
+                    break;
             }
         }
 
@@ -145,6 +145,100 @@ namespace ProcessorEmulator.Emulation
             {
                 programCounter += (uint)(offset << 2);
             }
+        }
+
+        private void ExecuteFloatingPoint(uint instruction)
+        {
+            uint fmt = (instruction >> 21) & 0x1F;
+            uint ft = (instruction >> 16) & 0x1F;
+            uint fs = (instruction >> 11) & 0x1F;
+            uint fd = (instruction >> 6) & 0x1F;
+            uint funct = instruction & 0x3F;
+
+            switch (funct)
+            {
+                case 0x00: // add.s
+                    floatingPointRegisters[fd] = floatingPointRegisters[fs] + floatingPointRegisters[ft];
+                    break;
+                case 0x01: // sub.s
+                    floatingPointRegisters[fd] = floatingPointRegisters[fs] - floatingPointRegisters[ft];
+                    break;
+                case 0x02: // mul.s
+                    floatingPointRegisters[fd] = floatingPointRegisters[fs] * floatingPointRegisters[ft];
+                    break;
+                case 0x03: // div.s
+                    floatingPointRegisters[fd] = floatingPointRegisters[fs] / floatingPointRegisters[ft];
+                    break;
+                default:
+                    throw new NotSupportedException($"Floating-point function {funct:X2} not supported.");
+            }
+        }
+
+        private void ExecuteDSPInstruction(uint instruction)
+        {
+            uint funct = instruction & 0x3F;
+            uint rs = (instruction >> 21) & 0x1F;
+            uint rt = (instruction >> 16) & 0x1F;
+            uint rd = (instruction >> 11) & 0x1F;
+
+            switch (funct)
+            {
+                case 0x20: // madd (Multiply-Add)
+                    registers[rd] += (uint)((int)registers[rs] * (int)registers[rt]);
+                    break;
+                case 0x21: // msub (Multiply-Subtract)
+                    registers[rd] -= (uint)((int)registers[rs] * (int)registers[rt]);
+                    break;
+                default:
+                    HandleException($"Unsupported DSP function: {funct:X2}");
+                    break;
+            }
+        }
+
+        private void ExecuteSystemInstruction(uint instruction)
+        {
+            uint funct = instruction & 0x3F;
+
+            switch (funct)
+            {
+                case 0x0C: // syscall
+                    HandleSyscall();
+                    break;
+                case 0x08: // jr (jump register)
+                    uint rs = (instruction >> 21) & 0x1F;
+                    programCounter = registers[rs];
+                    break;
+                default:
+                    throw new NotSupportedException($"System function {funct:X2} not supported.");
+            }
+        }
+
+        private void HandleSyscall()
+        {
+            uint syscallCode = registers[2]; // $v0 register
+            switch (syscallCode)
+            {
+                case 1: // Print integer
+                    Console.WriteLine(registers[4]); // $a0 register
+                    break;
+                case 4: // Print string
+                    uint address = registers[4];
+                    while (memory[address] != 0)
+                    {
+                        Console.Write((char)memory[address]);
+                        address++;
+                    }
+                    Console.WriteLine();
+                    break;
+                default:
+                    throw new NotSupportedException($"Syscall {syscallCode} not supported.");
+            }
+        }
+
+        private void HandleException(string message)
+        {
+            Console.WriteLine($"Exception: {message}");
+            // Implement exception handling logic here
         }
     }
 }
