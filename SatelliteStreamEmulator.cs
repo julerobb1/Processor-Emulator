cfe344851a7c0f58b8620073bf35382527006cad
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-06-24 10:29:46 -0500

    Update SatelliteStreamEmulator.cs

--- SatelliteStreamEmulator.cs
+++ SatelliteStreamEmulator.cs
@@ -5,13 +5,13 @@ namespace ProcessorEmulator.Emulation
     // Placeholder for satellite transport stream and keep-alive emulation
     public class SatelliteStreamEmulator
     {
-        public void InjectKeepAlivePacket()
+        public static void InjectKeepAlivePacket()
         {
             // Simulate NIT/ECM/EIT/authorization packets
             // Used to keep receiver firmware from deactivating
         }
 
-        public void FeedCustomData(byte[] tsPacket)
+        public static void FeedCustomData(byte[] tsPacket)
         {
             // Feed custom MPEG-TS or data packets to the receiver
         }
