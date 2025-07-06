f8d0f9378ae5587c929f51861fed7404067e6384
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-06 12:05:01 -0500

    Add Console.WriteLine stubs to emulator methods
    Each IEmulator implementation now logs method calls using Console.WriteLine for LoadBinary, Run, Step, Decompile, and Recompile. This provides basic tracing for stub emulator classes.

--- Emulation/StubEmulators.cs
+++ Emulation/StubEmulators.cs
@@ -1,112 +1,307 @@
+using System;
+
 namespace ProcessorEmulator.Emulation
 {
     // Do not redefine IEmulator here. It should be defined only once in your project.
 
     public class Sparc64Emulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("Sparc64Emulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("Sparc64Emulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("Sparc64Emulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("Sparc64Emulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("Sparc64Emulator: Recompile called with code: " + code); 
+        }
     }
     public class AlphaEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("AlphaEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("AlphaEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("AlphaEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("AlphaEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("AlphaEmulator: Recompile called with code: " + code); 
+        }
     }
     public class SuperHEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("SuperHEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("SuperHEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("SuperHEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("SuperHEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("SuperHEmulator: Recompile called with code: " + code); 
+        }
     }
     public class RiscV32Emulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("RiscV32Emulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("RiscV32Emulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("RiscV32Emulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("RiscV32Emulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("RiscV32Emulator: Recompile called with code: " + code); 
+        }
     }
     public class RiscV64Emulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("RiscV64Emulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("RiscV64Emulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("RiscV64Emulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("RiscV64Emulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("RiscV64Emulator: Recompile called with code: " + code); 
+        }
     }
     public class S390XEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("S390XEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("S390XEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("S390XEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("S390XEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("S390XEmulator: Recompile called with code: " + code); 
+        }
     }
     public class HppaEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("HppaEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("HppaEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("HppaEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("HppaEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("HppaEmulator: Recompile called with code: " + code); 
+        }
     }
     public class MicroBlazeEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("MicroBlazeEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("MicroBlazeEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("MicroBlazeEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("MicroBlazeEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("MicroBlazeEmulator: Recompile called with code: " + code); 
+        }
     }
     public class CrisEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("CrisEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("CrisEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("CrisEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("CrisEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("CrisEmulator: Recompile called with code: " + code); 
+        }
     }
     public class Lm32Emulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("Lm32Emulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("Lm32Emulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("Lm32Emulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("Lm32Emulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("Lm32Emulator: Recompile called with code: " + code); 
+        }
     }
     public class M68KEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("M68KEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("M68KEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("M68KEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("M68KEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("M68KEmulator: Recompile called with code: " + code); 
+        }
     }
     public class XtensaEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("XtensaEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("XtensaEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("XtensaEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("XtensaEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("XtensaEmulator: Recompile called with code: " + code); 
+        }
     }
     public class OpenRiscEmulator : IEmulator
     {
-        public void LoadBinary(byte[] binary) { }
-        public void Run() { }
-        public void Step() { }
-        public void Decompile() { }
-        public void Recompile(string code) { }
-}
-// Add more stubs as needed for additional architectures
-// Add more stubs as needed for additional architectures
-// If you have other architectures defined elsewhere, ensure they are not duplicated here.
+        public void LoadBinary(byte[] binary) 
+        { 
+            Console.WriteLine("OpenRiscEmulator: LoadBinary called."); 
+        }
+        public void Run() 
+        { 
+            Console.WriteLine("OpenRiscEmulator: Run called."); 
+        }
+        public void Step() 
+        { 
+            Console.WriteLine("OpenRiscEmulator: Step called."); 
+        }
+        public void Decompile() 
+        { 
+            Console.WriteLine("OpenRiscEmulator: Decompile called."); 
+        }
+        public void Recompile(string code) 
+        { 
+            Console.WriteLine("OpenRiscEmulator: Recompile called with code: " + code); 
+        }
+    }
+    // Add more stubs as needed for additional architectures
 }
