fd3f1695456d51e791858b4eb3997d8ad3871e95
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-06 11:56:45 -0500

    Expand emulator stubs with additional methods
    Refactored architecture emulator stubs to include Step, Decompile, and Recompile methods for each class. Removed redundant IEmulator interface definition and ensured all emulator stubs follow a consistent structure.

--- Emulation/StubEmulators.cs
+++ Emulation/StubEmulators.cs
@@ -1,33 +1,112 @@
-using System;
-
 namespace ProcessorEmulator.Emulation
 {
-    public interface IEmulator
-    {
-        void LoadBinary(byte[] binary);
-        void Run();
-    }
+    // Do not redefine IEmulator here. It should be defined only once in your project.
 
-    public class Mips32Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class Mips64Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class ArmEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class Arm64Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class PowerPcEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class X86Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class X64Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class SparcEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class Sparc64Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class AlphaEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class SuperHEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class RiscV32Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class RiscV64Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class S390XEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class HppaEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class MicroBlazeEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class CrisEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class Lm32Emulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class M68KEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class XtensaEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    public class OpenRiscEmulator : IEmulator { public void LoadBinary(byte[] b) { } public void Run() { } }
-    // Add more stubs as needed for additional architectures
+    public class Sparc64Emulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class AlphaEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class SuperHEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class RiscV32Emulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class RiscV64Emulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class S390XEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class HppaEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class MicroBlazeEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class CrisEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class Lm32Emulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class M68KEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class XtensaEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+    }
+    public class OpenRiscEmulator : IEmulator
+    {
+        public void LoadBinary(byte[] binary) { }
+        public void Run() { }
+        public void Step() { }
+        public void Decompile() { }
+        public void Recompile(string code) { }
+}
+// Add more stubs as needed for additional architectures
+// Add more stubs as needed for additional architectures
+// If you have other architectures defined elsewhere, ensure they are not duplicated here.
 }
