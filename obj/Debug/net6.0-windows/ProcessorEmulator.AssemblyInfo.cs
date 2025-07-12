1128c3a34be9683fdbabaf8f1b9b37b1bb600bc5
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-12 17:02:17 -0500

    Update assembly info and cache for new build
    Regenerated AssemblyInfo and cache files to reflect the latest build. No source code changes included.

--- obj/Debug/net6.0-windows/ProcessorEmulator.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/ProcessorEmulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2b3a1396ae7ea40ef050287c607fcf77943695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739b9829c68a63af58450edbf74d127bcd32a408")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
