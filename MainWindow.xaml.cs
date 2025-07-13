ef52ca4ffb3329a65a7fccc3ef0348d589defaa5
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-12 20:52:07 -0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using ProcessorEmulator.Tools.StubUnicorn.ProcessorEmulator.Tools; // Add this if BinaryTranslator is in Tools namespace
 
 namespace ProcessorEmulator
 {
