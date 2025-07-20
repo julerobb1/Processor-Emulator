12c77366671cd6b180757721c82cefd6bc1cd052
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-20 12:19:39 -0500

    Create FolderAnalysisWindow.xaml.cs

---
+++ FolderAnalysisWindow.xaml.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Windows;
+
+namespace ProcessorEmulator
+{
+    /// <summary>
+    /// Interaction logic for FolderAnalysisWindow.xaml
+    /// </summary>
+    public partial class FolderAnalysisWindow : Window
+    {
+        public FolderAnalysisWindow(List<FileRecord> items)
+        {
+            InitializeComponent();
+            DataGridFiles.ItemsSource = items;
+        }
+    }
+
+    public class FileRecord
+    {
+        public string FilePath { get; set; }
+        public long Size { get; set; }
+        public string HexPreview { get; set; }
+    }
+}
