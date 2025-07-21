7ae4834cd72266d6b4e1c6c796e0e8a1f23dfffa
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-21 12:50:10 -0500

    Update BinaryScanner.cs

--- BinaryScanner.cs
+++ BinaryScanner.cs
@@ -40,19 +40,5 @@ namespace ProcessorEmulator.Tools
         }
     }
 
-    internal static class Disassembler
-    {
-        public static string[] Disassemble(byte[] data, string arch)
-        {
-            // Stub implementation: return dummy lines for now
-            return new[]
-            {
-                $"Disassembly preview for arch: {arch}",
-                "0x0000: NOP",
-                "0x0001: MOV R1, R2",
-                "0x0002: ADD R3, R4",
-                "0x0003: JMP 0x0000"
-            };
-        }
-    }
+    // Removed internal stub Disassembler; using public Disassembler class
 }
