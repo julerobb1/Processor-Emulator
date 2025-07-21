615a7ff27c3781e7c62a1cbe1fc2e410f7c8c379
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-21 12:18:56 -0500

    Update FolderAnalysisWindow.cs

--- FolderAnalysisWindow.cs
+++ FolderAnalysisWindow.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.IO;
 using System.Linq;
 using System.Windows.Media.Imaging;
