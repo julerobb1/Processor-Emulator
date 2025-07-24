bd875ef8c59ef04b1b3b11f0f8ebab76bcc40a00
Julian R <25588072+julerobb1@users.noreply.github.com>
2025-07-24 10:28:27 -0500

    Update ProcessorEmulator.AssemblyInfo.cs

--- obj/Debug/net6.0-windows/ProcessorEmulator.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/ProcessorEmulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22eed77e2c7f1d949da0a494b13714c73c37f79a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37cb011286f4ed4feecdaaa47b582302f0271c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProcessorEmulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
