98b9b0c8c7e17d2dd85c5f8cc2e38cc0c59f3f46
Julian R <25588072+julerobb1@users.noreply.github.com>
2026-02-05 10:17:22 -0600

    Update UverseEmulator.cs

--- UverseEmulator.cs
+++ UverseEmulator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
+using ProcessorEmulator; // Added for IChipsetEmulator
 
 namespace ProcessorEmulator.Emulation
 {
