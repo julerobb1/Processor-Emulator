3f4e15e14e7389a35c2268b6d15e133c44d4ea7b
Julian R <25588072+julerobb1@users.noreply.github.com>
2026-02-24 09:03:07 -0600

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,7 +1,6 @@
-namespace ProcessorEmulator
-{
-    public class GlobalUsings
-    {
+// Global usings curated to avoid ambiguous references between WPF and WinForms
+// Only common, non-conflicting namespaces are included here. Specific UI
+// namespaces are added in individual files to maintain clarity.
 
-    }
-}
+global using System.Diagnostics;
+global using System.Text;
